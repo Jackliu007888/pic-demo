b85b4c983acdccbf82f683e763c547abb32c192e
jackliu007888 <jackliulovelt@outlook.com>
2017-10-30 00:42:05 +0800

    upload

--- upload.js
+++ upload.js
@@ -1,26 +1,32 @@
 var OSS = require('ali-oss')
 var co = require('co')
 var fs = require('fs')
+var config = require('./accessconfig')
 var jsonData = JSON.parse(fs.readFileSync('./dist/dddog.jpg.json'))
+var uploadErr = false
+var upNode = 0
 
-// console.log(jsonData[0])
-for (var i = 0; i < jsonData.length; i++) {
-  var uuid = jsonData[i].uuid
-  var data = jsonData[i].data
-  updateFile(uuid, data)
-}
-
-function updateFile(uuid, data) {
+upHandle(jsonData.data, {}, uploadErr, upNode)
+function upHandle(data, argcfg, uploadErr, upNode) {
   var client = new OSS({
-    region: 'oss-cn-shenzhen',
-    accessKeyId: '',
-    accessKeySecret: '',
-    bucket: 'pic-demo'
+    region: argcfg.region || config.region,
+    accessKeyId: argcfg.accessKeyId || config.accessKeyId,
+    accessKeySecret: argcfg.accessKeySecret || config.accessKeySecret,
+    bucket: argcfg.bucket || config.bucket
   })
   co(function* () {
-    var result = yield client.put('piece/' + uuid + '.txt', Buffer.from(data))
-    console.log(result)
+    for (var i = upNode; i < jsonData.data.length; i++) {
+      var uuid = jsonData.data[i].uuid
+      var data = jsonData.data[i].data
+      var result = yield client.put('piece/' + uuid + '.txt', Buffer.from(data))
+      console.log(result)
+      upNode = i
+    }
+  }).then(function (value) {
+    // 校验
+    console.log('全部分片上传成功')
   }, function (err) {
+    uploadErr = true
     console.error(err.stack)
   })
 }
